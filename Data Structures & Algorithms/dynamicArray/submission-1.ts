380fa90bdf476cead6ba95e6c1484c9be2c53d46
Adrian San Pedro <adrian.san.pedro@outlook.com>
2026-07-21 09:38:23 -0700

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/dynamicArray/submission-1.ts
@@ -0,0 +1,68 @@
+class DynamicArray {
+    private capacity = 0
+    private arr = []
+    /**
+     * @constructor
+     * @param {number} capacity
+     */
+    constructor(capacity: number) {
+        this.capacity = capacity
+    }
+
+    /**
+     * @param {number} i
+     * @returns {number}
+     */
+    get(i: number): number {
+        return this.arr[i]
+    }
+
+    /**
+     * @param {number} i
+     * @param {number} n
+     * @returns {void}
+     */
+    set(i: number, n: number): void {
+        this.arr[i] = n
+    }
+
+    /**
+     * @param {number} n
+     * @returns {void}
+     */
+    pushback(n: number): void {
+        if(this.arr.length >= this.capacity) {
+            this.resize()
+        }
+
+        this.arr.push(n)
+    }
+
+    /**
+     * @returns {number}
+     */
+    popback(): number {
+        return this.arr.pop()
+    }
+
+    /**
+     * @returns {void}
+     */
+    resize(): void {
+        this.capacity *= 2
+    }
+
+    /**
+     * @returns {number}
+     */
+    getSize(): number {
+        return this.arr.length
+    }
+
+    /**
+     * @returns {number}
+     */
+    getCapacity(): number {
+        return this.capacity
+    }
+}
